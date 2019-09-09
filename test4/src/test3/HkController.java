5f85479808c8cc2526af6b8f8015c79de9e8bb23
HKEDU <HKEDU@192.168.2.114>
2019-09-09 17:36:47 +0900

    HKController완성

--- test4/src/test3/HkController.java
+++ test4/src/test3/HkController.java
@@ -24,21 +24,21 @@ public class HkController extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		String command=request.getParameter("command");
 		HkDao dao=new HkDao();
-		
+
 		if(command.equals("boardlist")) { //글목록 보여주기
-		System.out.println("왜안될까?");
-		List<HkDto>list=dao.getAllList();
-		request.setAttribute("list", list);
-		request.getRequestDispatcher("boardlist.jsp").forward(request,response);
+			System.out.println("왜안될까?");
+			List<HkDto>list=dao.getAllList();
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("boardlist.jsp").forward(request,response);
 		}else if(command.equals("insert")) {
 			//여기서는 전송된 4개의 파라미터 id, name, title, content를 받는 코드 작성
 			String id= request.getParameter("id");
 			String name = request.getParameter("name");
 			String title=request.getParameter("title");
 			String content=request.getParameter("content");
-			
-			
-			
+
+
+
 			//4개의 파라미터를 이용해서 dao에 insert 메서드 실행----> 그래야 디비에 게시글이 저장됨
 			boolean isS=dao.insertBoard(new HkDto (id,name,title,content));
 			//그리고 글목록 페이지로 이동하는 코드 작성
@@ -56,27 +56,48 @@ public class HkController extends HttpServlet {
 			int seq=Integer.parseInt(request.getParameter("seq"));
 			boolean isS=dao.delBoard(seq);
 			if(isS) {
-						jsForward("HkController.do?command=boardlist","해당글을삭제합니다.",response);
+				jsForward("HkController.do?command=boardlist","해당글을삭제합니다.",response);
 			}else {
-						jsForward("HkController.do?command=boarddetail&seq="+seq,"해당글을삭제실패.",response);
-						
+				jsForward("HkController.do?command=boarddetail&seq="+seq,"해당글을삭제실패.",response);
+
 			}
-			
+
 		}else if(command.equals("updateform")) {
 			int seq=Integer.parseInt(request.getParameter("seq"));
-			
+			HkDto dto= dao.getBoard(seq);
+			request.setAttribute("dto", dto);
+			//					pageContext.forward("boardupdate.jsp")
+			dispatch("boardupdate.jsp",request,response);
+
 		}else if(command.equals("boardupdate")) {
 			String title=request.getParameter("title");
 			String content=request.getParameter("content");
 			int seq=Integer.parseInt(request.getParameter("seq"));
-			
-			
+
+			boolean isS=dao.updateBoard(new HkDto(seq,title,content));
+			if(isS) {
+				jsForward("HkController.do?command=boarddetail&seq="+seq,"글수정성공",response);
+			}else {
+				jsForward("HkController.do?command=updateform&seq="+seq,"글수정실패",response);
+
+			}
 		}else if(command.contentEquals("muldel")) {
 			String[] seqs=request.getParameterValues("chk");
-					
+			if(seqs==null||seqs.length==0) {
+				jsForward("HkController.do?command=boardlist","최소하나이상 체크하세요",response);
+			}else {
+				boolean isS=dao.muldel(seqs);
+				if(isS) {
+					jsForward("HkCotroller.do?command=boardlist","여러글삭제합니다", response);
+				}else {
+
+					jsForward("HkControllerdo?command=boardlist","여러글을삭제실패",response);
+				}
+
+			}
 		}
-			
-}//doPost()끝
+
+	}//doPost()끝
 
 	private void jsForward(String url, String msg,  HttpServletResponse response)throws IOException {
 		String str="<script type='text/javascript'>"
@@ -92,7 +113,7 @@ public class HkController extends HttpServlet {
 		RequestDispatcher dispatch=request.getRequestDispatcher(url);
 		dispatch.forward(request, response);
 	}
-	
+
 }//class끝
 
 
